928f6de9c1d8e73c8a8f0b8e8226b8a615c67d50
Yadija <ayuliadi01@gmail.com>
2023-08-12 19:58:09 +0800

    🎉initial: install nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
