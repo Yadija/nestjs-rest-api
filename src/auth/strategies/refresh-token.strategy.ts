d06afb04c7213261c082d2bc702b78cbf3634dda
Yadija <ayuliadi01@gmail.com>
2023-08-13 19:32:27 +0800

    ⚒️build: make jwt strategy

---
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,29 @@
+/* istanbul ignore file */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../types';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.REFRESH_TOKEN_KEY,
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req
+      ?.get('auhtorization')
+      ?.replace('Bearer', '')
+      .trim();
+
+    return { ...payload, refreshToken };
+  }
+}
